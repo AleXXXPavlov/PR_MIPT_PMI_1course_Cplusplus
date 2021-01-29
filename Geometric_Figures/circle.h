ad2c5bafc245d96cbb3330461e49976b32048986
tOpDeveLOpeR-pAs <71667213+tOpDeveLOpeR-pAs@users.noreply.github.com>
2021-01-29 17:01:52 +0300

    Add files via upload

---
+++ Geometric_Figures/circle.h
@@ -0,0 +1,26 @@
+
+#ifndef _CIRCLE_
+#define _CIRCLE_
+
+#include "ellipse.h"
+
+class Circle : public Ellipse {
+public:
+	Circle(Point, double);						// конструктор от центра и радиуса
+	~Circle() = default;
+	
+	double radius() const;						// получаем радиус круга
+};
+
+Circle::Circle(Point center, double radius) {
+	Ellipse::focus1 = center;
+	Ellipse::focus2 = center;
+
+	Ellipse::sMajorAxis = radius;
+}
+
+double Circle::radius() const {
+	return sMajorAxis;
+}
+
+#endif
